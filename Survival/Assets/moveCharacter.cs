d044943097cd9fa7f7b604899bfdcec16c253c9f
quiru <reacosta07@gmail.com>
2019-07-25 16:29:00 -0500

    create the world
    scene within nothing

---
+++ Survival/Assets/moveCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCharacter : MonoBehaviour
+{
+    public Vector3 pos;
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    
+    void Update()
+    {
+        bool aKey = Input.GetKey(KeyCode.A);
+        bool sKey = Input.GetKey(KeyCode.S);
+        bool wKey = Input.GetKey(KeyCode.W);
+        bool dKey = Input.GetKey(KeyCode.D);
+        transform.position = pos;
+    }
+}
