a8ea319d57b51c5167b315ab8bf5c842551ea80c
quiru <reacosta07@gmail.com>
2019-08-05 10:39:38 -0500

    character with movement
    add scritps to character for movement

---
+++ Survival/Assets/scripts/CamFps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFps : MonoBehaviour
+{
+    float mouseX; // variable para guardar valores del movimiento del mouse en X 
+    float mouseY; // variable para guardar valores del movimiento del mouse en Y
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X"); // captura el movimiento del mouse y se lo suma a mouseX para el movimiento de la camara en x
+        mouseY -= Input.GetAxis("Mouse Y"); // captura el movimiento del mouse y se lo resta a mouseY para el movimiento de la camara en y
+
+        if (mouseY > 40) // condicion para que no mire mas de 40 hacia arriba y menos de 40 hacia abajo (para que no gire la camara en y)
+        {
+            mouseY = 40;
+        }
+        else if (mouseY < -40)
+        {
+            mouseY = -40;
+        }
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0); //mueve la camara segun el vaor que alla en
+    }
+}
---
+++ Survival/Assets/scripts/MovFps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovFps : MonoBehaviour
+{
+    public float speed = 0.1f; // para darle una velocidad al movimiento
+    float mouseX; // para capturar la entrada del mouse
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X"); // va agregando numeros segun el movimiento del mouse en x
+        transform.eulerAngles = new Vector3(0, mouseX, 0); // mueve la camara segun el vaor que alla en mouseX
+
+
+        if (Input.GetKey(KeyCode.W)) //si se presiona la tecla w
+        {
+            transform.position += transform.forward * speed; //se mueve hacia adelante
+        }
+        if (Input.GetKey(KeyCode.S)) //si se presiona la tecla s
+        {
+            transform.position -= transform.forward * speed; //se mueve hacia atras
+        }
+        if (Input.GetKey(KeyCode.A)) // si se presiona la tecla a
+        {
+            transform.position -= transform.right * speed; // se mueve a la izquierda
+        }
+        if (Input.GetKey(KeyCode.D)) // si se presiona la tecla d
+        {
+            transform.position += transform.right * speed; // se mueve hacia la derecha
+        }
+    }
+}
